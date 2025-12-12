ad6db75284b7768710d064a107c221138c627773
nadun96 <naadunudaraka@gmail.com>
2025-12-12 17:25:16 +0530

    added froms and reactive forms modules

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { ReservationFromComponent } from '../reservation-from/reservation-from.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     ReservationListComponent,
-    ReservationFromComponent
+    ReservationFromComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReservationModule { }
