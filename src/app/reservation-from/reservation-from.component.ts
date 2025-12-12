a485d48029603dfbd35adb21cec387ee425cb961
nadun96 <naadunudaraka@gmail.com>
2025-12-12 18:04:54 +0530

    form validators

--- src/app/reservation-from/reservation-from.component.ts
+++ src/app/reservation-from/reservation-from.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-from',
   templateUrl: './reservation-from.component.html',
   styleUrls: ['./reservation-from.component.css']
 })
-export class ReservationFromComponent {
-  reservationForm = new FormGroup({
-    fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    checkInDate: new FormControl('', Validators.required),
-    checkOutDate: new FormControl('', Validators.required),
-    numberOfGuests: new FormControl(1, [Validators.required, Validators.min(1)])
-  });
+export class ReservationFromComponent implements OnInit {
+
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      guestName: ['', [Validators.required, Validators.minLength(3)]],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      roomNumber: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
 
   onSubmit() {
-
+    console.log(this.reservationForm.valid);
   }
+
 }
